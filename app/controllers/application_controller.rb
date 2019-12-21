d0a679facaf92d1da233d0998324985c7fa96f3b
Nethelbert <gilblasse@gmail.com>
2019-12-21 04:49:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+    binding.pry
     if params[:username] != "" && params[:password] != ""
        User.create(username: params[:username], password: params[:password])
        redirect '/login'
